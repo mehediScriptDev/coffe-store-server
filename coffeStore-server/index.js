5e2a75b8884021e9486a3425ccda96d0f58bd0f6
Mehedi Mehedi <mehedi25122003@gmail.com>
2025-05-11 21:41:45 +0600

    new mongouser created

--- coffeStore-server/index.js
+++ coffeStore-server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -11,37 +11,6 @@ app.use(express.json());
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USR}:${process.env.DB_PASS}@cluster0.wld9ndi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-  tls: true
-});
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-
-    app.post('/coffe', async(req,res)=>{
-      const product = req.body;
-      console.log('product')
-    })
-    
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
-}
-run().catch(console.dir);
 
 app.get('/', (req,res)=>{
     res.send('coffe maker is running.')

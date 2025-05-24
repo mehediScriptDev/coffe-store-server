f0701e338ca0f78b270cdb20d0c367b1249a4e1f
Mehedi Mehedi <mehedi25122003@gmail.com>
2025-05-24 18:42:16 +0600

    coffe delete done

--- coffeStore-server/index.js
+++ coffeStore-server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -38,6 +38,13 @@ async function run() {
     app.post('/coffe', async(req,res)=>{
       const newCoffe =req.body;
       const result = await coffeCollection.insertOne(newCoffe);
+      res.send(result);
+    })
+
+    app.delete('/coffe/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result= await coffeCollection.deleteOne(query);
       res.send(result);
     })
 
